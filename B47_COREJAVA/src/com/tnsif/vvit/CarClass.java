37a43643d6b476956e3ead776d017e0a45ca57a7
Admin <Admin@DESKTOP-2NE76QA>
2023-03-17 15:12:47 +0530

    First Commit

---
+++ B47_COREJAVA/src/com/tnsif/vvit/CarClass.java
@@ -0,0 +1,21 @@
+package com.tnsif.vvit;
+/*
+ * Program to demonstrate class and objects with data encapsulation
+ */
+public class CarClass {
+	// Data members for the class
+	int model;
+	String color;
+	int reg=111;
+	int amt=10000;
+	// Member functions for the class
+	void forward() {
+		System.out.println("Car is moving in forward direction");
+	}
+	void backward() {
+		System.out.println("Car is moving in backward direction");
+	}
+	void stop() {
+		System.out.println("Car is not moving");
+	}
+}
---
+++ B47_COREJAVA/src/com/tnsif/vvit/Exceutor.java
@@ -0,0 +1,20 @@
+package com.tnsif.vvit;
+
+import java.util.Scanner;
+
+public class Exceutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CarClass ob = new CarClass();// creation of object
+		ob.forward();// calling the member function of the CarClass
+		ob.backward();
+		ob.stop();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Amount is "+ob.amt);
+		
+		System.out.println("REG is "+ob.reg);
+	}
+
+}
